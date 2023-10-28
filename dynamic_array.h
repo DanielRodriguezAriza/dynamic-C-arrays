5ba23aa93a07a0d48a8fb6bdca09522e904ae9c1
PachecoAlFredo <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-10-28 23:27:11 +0200

    Fixed translation unit issues
    Fixed an oversight where including the vector in different files would include the source code for the functions in multiple translation units (obviously...)
    
    Added an stb style macro so that the user can decide in which translation unit they want the implementation to be defined.

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,60 +1,54 @@
 #ifndef DRA_DYNAMIC_ARRAY_H
 #define DRA_DYNAMIC_ARRAY_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
 
 #define dynamic_array(type) struct {type *data; size_t length, capacity;}
 
-#define ARR_CAST_PTR(type, x) ((dynamic_array(type)*)(x))
 
-void dynamic_array_set_info(void *arr, void *ptr, size_t len, size_t cap)
-{
-	ARR_CAST_PTR(void,arr)->data = ptr;
-	ARR_CAST_PTR(void,arr)->length = len;
-	ARR_CAST_PTR(void,arr)->capacity = cap;
-}
+typedef dynamic_array(char) d_arr;
+typedef dynamic_array(char) *d_arr_ptr;
 
-int dynamic_array_allocate_bytes(void *arr, size_t bytes)
-{
-	void *tmp = (void*)calloc(bytes, 1);
-	dynamic_array_set_info(arr, tmp, 0, tmp == NULL? 0 : bytes);
-	return tmp == NULL;
-}
+typedef dynamic_array(int8_t) d_arr_i8;
+typedef dynamic_array(uint8_t) d_arr_u8;
+
+typedef dynamic_array(int16_t) d_arr_i16;
+typedef dynamic_array(uint16_t) d_arr_u16;
+
+typedef dynamic_array(int32_t) d_arr_i32;
+typedef dynamic_array(uint32_t) d_arr_u32;
+
+typedef dynamic_array(int64_t) d_arr_i64;
+typedef dynamic_array(uint64_t) d_arr_u64;
+
+typedef dynamic_array(float) d_arr_f32;
+typedef dynamic_array(double) d_arr_f64;
+
+
+void dynamic_array_set_info(void *arr, void *ptr, size_t len, size_t cap);
+void dynamic_array_init(void *arr); /* used to initialize dynamic as empty arrays and make the first dynamic memory allocation when performing the first push operation */
+
+int dynamic_array_allocate_bytes(void *arr, size_t bytes);
+void dynamic_array_deallocate(void *arr);
+
+int dynamic_array_reallocate(void *arr, size_t new_cap);
+int dynamic_array_has_to_reallocate(void *arr);
 
 #define dynamic_array_allocate_elements_by_type(arr,type,count) dynamic_array_allocate_bytes(arr,sizeof(type) * count)
 
 #define dynamic_array_allocate_elements(arr,count) dynamic_array_allocate_bytes(arr,sizeof(((*arr).data[0])) * count)
 
-void dynamic_array_deallocate(void *arr)
-{
-	free(ARR_CAST_PTR(void,arr)->data);
-	dynamic_array_set_info(arr,NULL,0,0);
-}
+
 
 #define dynamic_array_get_element_at(arr, idx) ((*arr).data[idx])
 #define dynamic_array_get_element_at_by_type(arr, type, idx) (((type*)((*arr).data))[idx])
 
-int dynamic_array_reallocate(void *arr, size_t new_cap) //maybe rename to _reallocate_bytes?
-{
-	void *temp_ptr = realloc(ARR_CAST_PTR(void, arr)->data, new_cap);
-	if(temp_ptr == NULL)
-	{
-		return 1;
-	}
-	ARR_CAST_PTR(void,arr)->data = temp_ptr;
-	ARR_CAST_PTR(void,arr)->capacity = new_cap;
-	if(ARR_CAST_PTR(void,arr)->length > ARR_CAST_PTR(void,arr)->capacity)
-	{
-		ARR_CAST_PTR(void,arr)->length = ARR_CAST_PTR(void,arr)->capacity;
-	}
-	return 0;
-}
+/* redundant macros that exist for eyecandy */
+#define dynamic_array_get_element(arr, idx) dynamic_array_get_element_at(arr, idx)
+#define dynamic_array_get_element_by_type(arr, type, idx) dynamic_array_get_element_at_by_type(arr, type, idx)
 
-int dynamic_array_has_to_reallocate(void *arr)
-{
-	return ARR_CAST_PTR(void, arr)->length >= ARR_CAST_PTR(void, arr)->capacity;
-}
 
 
 #define dynamic_array_push_internal(arr, elem) ((arr)->data[dynamic_array_get_length(arr)] = (elem), (arr)->length += sizeof(((arr)->data)[0]),  0)
@@ -76,4 +70,60 @@ int dynamic_array_has_to_reallocate(void *arr)
 
 #define dynamic_array_resize_by_type(arr, type, new_size) dynamic_array_reallocate((arr), new_size * sizeof(type))
 
-#endif
+#define dynamic_array_get_data_pointer(arr) ((arr)->data)
+#define dynamic_array_get_data_pointer_by_type(arr, type) ((type*)((arr)->data))
+
+
+
+#ifdef DRA_DYNAMIC_ARRAY_IMPL
+
+void dynamic_array_set_info(void *arr, void *ptr, size_t len, size_t cap)
+{
+	((d_arr_ptr)(arr))->data = ptr;
+	((d_arr_ptr)(arr))->length = len;
+	((d_arr_ptr)(arr))->capacity = cap;
+}
+
+void dynamic_array_init(void *arr)
+{
+	dynamic_array_set_info(arr,NULL,0,0);
+}
+
+int dynamic_array_allocate_bytes(void *arr, size_t bytes)
+{
+	void *tmp = (void*)calloc(bytes, 1);
+	dynamic_array_set_info(arr, tmp, 0, tmp == NULL? 0 : bytes);
+	return tmp == NULL;
+}
+
+void dynamic_array_deallocate(void *arr)
+{
+	free(((d_arr_ptr)(arr))->data);
+	dynamic_array_set_info(arr,NULL,0,0);
+}
+
+int dynamic_array_reallocate(void *arr, size_t new_cap)
+{
+	void *temp_ptr = realloc(((d_arr_ptr)(arr))->data, new_cap);
+	if(temp_ptr == NULL)
+	{
+		return 1;
+	}
+	((d_arr_ptr)(arr))->data = temp_ptr;
+	((d_arr_ptr)(arr))->capacity = new_cap;
+	if(((d_arr_ptr)(arr))->length > ((d_arr_ptr)(arr))->capacity)
+	{
+		((d_arr_ptr)(arr))->length = ((d_arr_ptr)(arr))->capacity;
+	}
+	return 0;
+}
+
+int dynamic_array_has_to_reallocate(void *arr)
+{
+	return (((d_arr_ptr)(arr))->length >= ((d_arr_ptr)(arr))->capacity) || (((d_arr_ptr)(arr))->capacity == 0);
+}
+
+#endif /* DRA_DYNAMIC_ARRAY_IMPL */
+
+
+#endif /* DRA_DYNAMIC_ARRAY_H */
